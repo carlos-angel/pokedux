2795bf55f213e891f10a186a85513740bf6f199c
carlos alberto angel angel <c.a.a.a.503@gmail.com>
2022-09-07 21:48:09 -0500

    style: agregar el logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import { Col } from 'antd';
 import { PokemonList } from './components/PokemonList';
 import Searcher from './components/Searcher';
+import logo from './static/logo.svg';
 
 function App() {
   return (
     <div className='app'>
+      <Col span={4} offset={10}>
+        <img src={logo} alt='Pokedux' />
+      </Col>
       <Col span={8} offset={8}>
         <Searcher />
       </Col>
