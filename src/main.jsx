b57732762e67f0291d4722fccf0abdb463b0e033
carlos alberto angel angel <c.a.a.a.503@gmail.com>
2022-09-09 10:01:03 -0500

    refactor: quitar middleware pokemonsWithId

--- src/main.jsx
+++ src/main.jsx
@@ -5,12 +5,11 @@ import pokemonsReducer from './reducers/pokemons.reducer';
 import { Provider } from 'react-redux';
 import { legacy_createStore as createStore, compose, applyMiddleware } from 'redux';
 import './index.css';
-import { loggerMiddleware, pokemonsWithId } from './middlewares';
+import { loggerMiddleware } from './middlewares';
 
 const composedEnhancers = compose(
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
   applyMiddleware(loggerMiddleware),
-  applyMiddleware(pokemonsWithId),
 );
 
 const store = createStore(pokemonsReducer, composedEnhancers);
